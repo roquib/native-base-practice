2a2bb85799e6d45cf209eb532a90d4f05de258da
Abdur Roquib Pramanik <roquib01@outlook.com>
2022-04-13 16:38:48 +0600

    Add view

--- storybook/stories/index.ts
+++ storybook/stories/index.ts
@@ -1,3 +1,4 @@
 export * from "./components/basic/FlatList";
 export * from "./components/basic/SectionList";
 export * from "./components/basic/StatusBar";
+export * from "./components/basic/View";
