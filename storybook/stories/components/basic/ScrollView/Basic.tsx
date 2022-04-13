6e89c35bbf4329924dad90b9986868d5e635d68a
Abdur Roquib Pramanik <roquib01@outlook.com>
2022-04-13 16:43:26 +0600

    Add scroll view

---
+++ storybook/stories/components/basic/ScrollView/Basic.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { ScrollView, VStack, Center, useTheme, Heading } from "native-base";
+export const Example = () => {
+  const { colors } = useTheme();
+  return (
+    <ScrollView
+      maxW="300"
+      h="80"
+      _contentContainerStyle={{
+        px: "20px",
+        mb: "4",
+        minW: "72",
+      }}
+    >
+      <Center mt="3" mb="4">
+        <Heading fontSize="xl">Cyan</Heading>
+      </Center>
+      <VStack flex="1">
+        {Object.keys(colors.cyan).map((key, index) => {
+          if (index >= 1 && index <= 5)
+            return (
+              <Center py="4" bg={`cyan.${key}`}>
+                {key}
+              </Center>
+            );
+        })}
+      </VStack>
+      <Center mt="8" mb="4">
+        <Heading fontSize="xl">Yellow</Heading>
+      </Center>
+      <VStack flex="1">
+        {Object.keys(colors.cyan).map((key, index) => {
+          if (index >= 1 && index <= 5)
+            return (
+              <Center py="4" bg={`yellow.${key}`}>
+                {key}
+              </Center>
+            );
+        })}
+      </VStack>
+      <Center mt="8" mb="4">
+        <Heading fontSize="xl"> Violet</Heading>
+      </Center>
+      <VStack flex="1">
+        {Object.keys(colors.violet).map((key, index) => {
+          if (index >= 1 && index <= 5)
+            return (
+              <Center py="4" bg={`violet.${key}`}>
+                {key}
+              </Center>
+            );
+        })}
+      </VStack>
+      <Center mt="10" mb="4">
+        <Heading fontSize="2xl">Green</Heading>
+      </Center>
+      <VStack flex="1">
+        {Object.keys(colors.green).map((key, index) => {
+          if (index >= 1 && index <= 5)
+            return (
+              <Center py="4" bg={`green.${key}`}>
+                {key}
+              </Center>
+            );
+        })}
+      </VStack>
+    </ScrollView>
+  );
+};
--- storybook/stories/index.ts
+++ storybook/stories/index.ts
@@ -2,3 +2,4 @@ export * from "./components/basic/FlatList";
 export * from "./components/basic/SectionList";
 export * from "./components/basic/StatusBar";
 export * from "./components/basic/View";
+export * from "./components/basic/ScrollView";
